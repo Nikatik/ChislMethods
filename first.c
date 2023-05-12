1fa37d849e1e639a22e1fbb0f16f9ab721461ace
Nikita Sergeev <nikita.sergeev@math.msu.ru>
2023-05-13 00:24:14 +0300

    Formating first.c

--- first.c
+++ first.c
@@ -7,32 +7,38 @@ int func1 (void)
 #pragma GCC diagnostic ignored "-Wfloat-equal"
     for (int i = 0;; i++)
     {
-        if ((__float128)1. + eps == (__float128)1.)        //-Wfloat-equal
+        if ((__float128) 1. + eps == (__float128) 1.)        //-Wfloat-equal
         {
 #pragma GCC diagnostic pop
             return i;
         }
-        eps = (__float128)ldexpl ((long double)1., -i);
+        eps = (__float128) ldexpl ((long double) 1., -i);
     }
     return 0;
 }
 
 __float128 func2 (void)
 {
-    __float128 x = (__float128)0.;
+    __float128 x = (__float128) 0.;
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wfloat-equal"
-    for (int i = 0; x + (__float128)1. != x; i++) { x += (__float128)ldexpl ((long double)2., i / 20); }
+    for (int i = 0; x + (__float128) 1. != x; i++)
+    {
+        x += (__float128) ldexpl ((long double) 2., i / 20);
+    }
 #pragma GCC diagnostic pop
     return x;
 }
 
 __float128 func3 (void)
 {
-    __float128 x = (__float128)0.;
+    __float128 x = (__float128) 0.;
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wfloat-equal"
-    for (int i = 0; x + (__float128)pow (10., 20) != x; i++) { x += (__float128)ldexpl ((long double)2., i / 20); }
+    for (int i = 0; x + (__float128) pow (10., 20) != x; i++)
+    {
+        x += (__float128) ldexpl ((long double) 2., i / 20);
+    }
 #pragma GCC diagnostic pop
     return x;
 }
